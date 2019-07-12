a24ea3015dc9f3a06824eb3ac435473d91787747
Daryl <daryl@briviant.com>
2019-07-12 06:41:04 -0700

    added root module and browser module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,2 +1,19 @@
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
+@NgModule(
+            {
+                
+                imports: [
+
+                    BrowserModule
+
+                ],
+
+                declarations: [],
+
+                bootstrap: []
+
+            }
+         )
+export class AppModule {}

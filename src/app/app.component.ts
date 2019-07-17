0d42872fb6c0ff41797aca479eb033ff5d90f81a
Daryl <daryl@briviant.com>
2019-07-17 05:15:04 -0700

    added selector and template for appcomponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,14 @@
 import { Component } from '@angular/core';
 
-@Component()
+@Component(
+    
+            {
+
+                selector: 'app-root',
+
+                template: '<h1>My App</h1>'
+
+            }
+        
+          )
 export class AppComponent {}

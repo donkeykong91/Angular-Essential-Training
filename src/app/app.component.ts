a1789d67158bb7f01d53b16958d01c30301de0e8
Daryl <daryl@briviant.com>
2019-07-28 07:02:55 -0700

    added component property w/ @input decorator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,18 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
-@Component(
-    
-            {
-
-                selector: 'mw-app',
-
-                templateUrl: './app.component.html',
-
-                styleUrls: ['./app.component.css']
-
-            }
-        
-          )
-export class AppComponent {}
+@Component({
+  selector: "mw-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
+})
+export class AppComponent {
+  firstMediaItem = {
+    id: 1,
+    name: 'Firebug',
+    medium: 'Series',
+    category: 'Science Fiction',
+    year: 2010,
+    watchedOn: 1294166565384,
+    isFavorite: false
+  };
+}

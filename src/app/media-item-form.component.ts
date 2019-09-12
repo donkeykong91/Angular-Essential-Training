e5409da04d91d8de44e2b062d497b470ccf45e92
Daryl <daryl@briviant.com>
2019-09-12 16:13:34 -0700

    added custom form validtor

--- src/app/media-item-form.component.ts
+++ src/app/media-item-form.component.ts
@@ -13,15 +13,25 @@ export class MediaItemFormComponent implements OnInit {
     this.form = new FormGroup({
       medium: new FormControl("Movies"),
       name: new FormControl(
-        '',
+        "",
         Validators.compose([
           Validators.required,
           Validators.pattern("[\\w\\-\\s\\/]+")
         ])
       ),
       category: new FormControl(""),
-      year: new FormControl("")
+      year: new FormControl("", this.yearValidator)
     });
+  }
+  yearValidator(control: FormControl) {
+    if (control.value.trim().length === 0) return null;
+
+    const year = parseInt(control.value, 10);
+    const minYear = 1900;
+    const maxYear = 2100;
+
+    if (year >= minYear && year <= maxYear) return null;
+    else return { year: true };
   }
   onSubmit(mediaItem) {
     console.log(mediaItem);

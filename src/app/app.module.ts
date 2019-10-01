da1cc3f330c99441be02cb21847904f27b38e9d2
Daryl <daryl@briviant.com>
2019-10-01 06:13:09 -0700

    added provider in ngmodule to add mediaitemservice

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { FavoriteDirective } from "./favorite.directive";
 import { CategoryListPipe } from "./category-list.pipe";
 import { ReactiveFormsModule } from '@angular/forms';
 import { MediaItemFormComponent } from './media-item-form.component';
+import { MediaItemService } from './media-item.service';
 
 @NgModule({
   imports: [
@@ -21,6 +22,9 @@ import { MediaItemFormComponent } from './media-item-form.component';
     CategoryListPipe,
     MediaItemFormComponent
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  providers: [
+    MediaItemService
+  ]
 })
 export class AppModule {}

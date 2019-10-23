2c440f832da6c158e110eac7ed11f179f356624f
Daryl <daryl@briviant.com>
2019-10-23 05:13:36 -0700

    imported httpclientmodule and added httpclientmodule to imports metadata property array

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from "./app.component";
 import { MediaItemComponent } from "./media-item.component";
 import { MediaItemListComponent } from "./media-item-list.component";
@@ -13,7 +14,8 @@ import { lookupListToken, lookupLists } from './providers';
 @NgModule({
   imports: [
     BrowserModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   declarations: [
     AppComponent,

2cf4f828fffa50b36ea7b61e40d26ac1cbbb4965
Daryl <daryl@briviant.com>
2019-11-04 10:23:51 -0800

    imported MediaItemFormComponent to use in appRoutes obj

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,3 +1,6 @@
 import { Routes } from "@angular/router";
+import {MediaItemFormComponent} from './media-item-form.component'
 
-const appRoutes: Routes = [];
+const appRoutes: Routes = [
+    {path: 'add', component: MediaItemFormComponent}
+];
